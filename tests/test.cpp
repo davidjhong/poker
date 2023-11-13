c4358d487c06f790670ec4abaad1a43da230ac95
David Hong <dhong050@xe-01.cs.ucr.edu>
2023-11-12 23:43:26 -0800

    Add HighCard Test

--- tests/test.cpp
+++ tests/test.cpp
@@ -26,6 +26,24 @@ class StubHand {
       }
 };
 
+TEST(HandRankTests, hasHighCardTest) {
+    vector<Card*> testCards;
+    Card* card1 = new Card(1, "Spades", "One of Spades");
+    Card* card2 = new Card(5, "Spades", "Five of Spades");
+    Card* card3 = new Card(3, "Hearts", "Three of Hearts");
+    Card* card4 = new Card(9, "Spades", "Nine of Spades");
+    testCards.push_back(card1);
+    testCards.push_back(card2);
+    testCards.push_back(card3);
+    testCards.push_back(card4);
+
+    StubHand* testHand = new StubHand(testCards);
+    HandRank* handRanker = new HandRank();
+    
+    EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 50);
+}
+
+
 TEST(HandRankTests, hasPairTest) {
 
     vector<Card*> testCards;
@@ -60,6 +78,8 @@ TEST(HandRankTests, hasTwoPairTest) {
     
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 150);
 }
+
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
