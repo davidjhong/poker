73dbd15fcc100ffd3b22f8bf7ed3528402732e88
jasonan18 <anjason18@gmail.com>
2023-11-12 17:20:11 -0800

    Added Pot Class

---
+++ Pot.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <Player.h>
+#include <vector>
+
+
+class Pot {
+ public: 
+    Pot();
+    ~Pot();
+    const unsigned int getPot();
+
+ private:
+    void addToPot(const unsigned int);
+    void splitPot(const vector<Player>&);
+    int totalPot = 0;
+};
