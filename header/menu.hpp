13b71b1cdbbbe097fc0cb8ee60c4ec0aa90e929c
Chloe Tang <ctang085@xe-01.cs.ucr.edu>
2023-11-20 22:50:49 -0800

    updating class

--- header/menu.hpp
+++ header/menu.hpp
@@ -8,14 +8,25 @@ using namespace std;
 
 class Display
 {
+    private:
+    string playerName;
+    int balance;
+    Player* player;
+
     public:
         void displayMenu(ostream& out);
         void displayCardRankings(ostream& out);
         void displayRules(ostream& out);
         void displayCardCombinations(ostream& out);
-        
+        void displayPlayerBets(ostream& out, Player* player);
+        string getName()
+        {
+            return playerName;
+        }
+        int getBalance()
+        {
+            return balance;
+        }
 };
-
-    
 
 #endif
