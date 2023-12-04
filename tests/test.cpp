f522bb0766af40ebd79e29b8f3992ece611b9c4f
David Hong <dhong050@xe-01.cs.ucr.edu>
2023-12-03 18:24:01 -0800

    Create Test Cases with existing functions instead of stub hand

--- tests/test.cpp
+++ tests/test.cpp
@@ -100,21 +100,20 @@ TEST(HandRankTests, hasHighCardTest) {
 
 
 TEST(HandRankTests, hasPairTest) {
-
-    vector<Card*> testCards;
+    Hand* testHand = new Hand();
     Card* card1 = new Card(1, "Spades", "One of Spades", "♠");
-    Card* card2 = new Card(5, "Spades", "Five of Spades", "♠");
+    Card* card2 = new Card(9, "Hearts", "Nine of Hearts", "♥");
     Card* card3 = new Card(3, "Hearts", "Three of Hearts", "♥");
     Card* card4 = new Card(9, "Spades", "Nine of Spades", "♠");
-    testCards.push_back(card1);
-    testCards.push_back(card2);
-    testCards.push_back(card3);
-    testCards.push_back(card4);
+    Card* card5 = new Card(10, "Spades", "Ten of Spades", "♠");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
 
-    StubHand* testHand = new StubHand(testCards);
-    HandRank* handRanker = new HandRank();
+    vector<Card*> communityCards = {card3,card4,card5};
     
-    EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 9);
+    testHand->calculateStrength(communityCards);
+    
+    EXPECT_EQ(testHand->getStrength(), 59);
 }
 
 TEST(HandRankTests, hasTwoPairTest) {
@@ -134,6 +133,23 @@ TEST(HandRankTests, hasTwoPairTest) {
     HandRank* handRanker = new HandRank();
     
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 105);
+}
+
+TEST(HandRankTests, hasTwoPairTest2) { // Testing with real functions instead of stubhand
+    Hand* testHand = new Hand();
+    Card* card1 = new Card(1, "Spades", "One of Spades", "♠");
+    Card* card2 = new Card(9, "Hearts", "Nine of Hearts", "♥");
+    Card* card3 = new Card(10, "Hearts", "Ten of Hearts", "♥");
+    Card* card4 = new Card(9, "Spades", "Nine of Spades", "♠");
+    Card* card5 = new Card(10, "Spades", "Ten of Spades", "♠");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
+
+    vector<Card*> communityCards = {card3,card4,card5};
+    
+    testHand->calculateStrength(communityCards);
+    
+    EXPECT_EQ(testHand->getStrength(), 110);
 }
 
 TEST(HandRankTests, hasThreeOfKindTest) {
@@ -156,6 +172,23 @@ TEST(HandRankTests, hasThreeOfKindTest) {
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 151);
 }
 
+TEST(HandRankTests, hasThreeOfKindTest2) { // Testing with real functions instead of stubhand
+    Hand* testHand = new Hand();
+    Card* card1 = new Card(5, "Spades", "Five of Spades", "♠");
+    Card* card2 = new Card(5, "Hearts", "Five of Hearts", "♥");
+    Card* card3 = new Card(9, "Hearts", "Nine of Hearts", "♥");
+    Card* card4 = new Card(5, "Clubs", "Five of Clubs", "♣");
+    Card* card5 = new Card(10, "Spades", "Ten of Spades", "♠");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
+
+    vector<Card*> communityCards = {card3,card4,card5};
+    
+    testHand->calculateStrength(communityCards);
+    
+    EXPECT_EQ(testHand->getStrength(), 155);
+}
+
 TEST(HandRankTests, hasStraightTest) {
     vector<Card*> testCards;
     Card* card1 = new Card(1, "Spades", "One of Spades", "♠");
@@ -173,6 +206,24 @@ TEST(HandRankTests, hasStraightTest) {
     HandRank* handRanker = new HandRank();
     
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 205);
+}
+
+TEST(HandRankTests, hasStraightTest2) { // Testing with real functions instead of stubhand
+    Hand* testHand = new Hand();
+    Card* card1 = new Card(5, "Spades", "Five of Spades", "♠");
+    Card* card2 = new Card(6, "Hearts", "Six of Hearts", "♥");
+    Card* card3 = new Card(7, "Hearts", "Seven of Hearts", "♥");
+    Card* card4 = new Card(8, "Clubs", "Eight of Clubs", "♣");
+    Card* card5 = new Card(9, "Spades", "Nine of Spades", "♠");
+    Card* card6 = new Card(11, "Spades", "Jack of Spades", "♠");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
+
+    vector<Card*> communityCards = {card3,card4,card5,card6};
+    
+    testHand->calculateStrength(communityCards);
+    
+    EXPECT_EQ(testHand->getStrength(), 209);
 }
 
 TEST(HandRankTests, hasFullHouse) {
@@ -196,6 +247,24 @@ TEST(HandRankTests, hasFullHouse) {
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 308);
 }
 
+TEST(HandRankTests, hasFullHouseTest2) { // Testing with real functions instead of stubhand
+    Hand* testHand = new Hand();
+    Card* card1 = new Card(5, "Spades", "Five of Spades", "♠");
+    Card* card2 = new Card(5, "Hearts", "Five of Hearts", "♥");
+    Card* card3 = new Card(2, "Hearts", "Two of Hearts", "♥");
+    Card* card4 = new Card(5, "Clubs", "Five of Clubs", "♣");
+    Card* card5 = new Card(2, "Spades", "Two of Spades", "♠");
+    Card* card6 = new Card(11, "Spades", "Jack of Spades", "♠");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
+
+    vector<Card*> communityCards = {card3,card4,card5,card6};
+    
+    testHand->calculateStrength(communityCards);
+    
+    EXPECT_EQ(testHand->getStrength(), 307);
+}
+
 TEST(HandRankTests, hasFourOfKind) {
     vector<Card*> testCards;
     Card* card1 = new Card(2, "Spades", "Two of Spades", "♠");
@@ -215,6 +284,24 @@ TEST(HandRankTests, hasFourOfKind) {
     HandRank* handRanker = new HandRank();
     
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 352);
+}
+
+TEST(HandRankTests, hasFourOfKindTest2) { // Testing with real functions instead of stubhand
+    Hand* testHand = new Hand();
+    Card* card1 = new Card(5, "Spades", "Five of Spades", "♠");
+    Card* card2 = new Card(5, "Hearts", "Five of Hearts", "♥");
+    Card* card3 = new Card(2, "Hearts", "Two of Hearts", "♥");
+    Card* card4 = new Card(5, "Clubs", "Five of Clubs", "♣");
+    Card* card5 = new Card(2, "Spades", "Two of Spades", "♠");
+    Card* card6 = new Card(5, "Diamonds", "Five of Diamonds", "♦");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
+
+    vector<Card*> communityCards = {card3,card4,card5,card6};
+    
+    testHand->calculateStrength(communityCards);
+    
+    ASSERT_EQ(testHand->getStrength(), 355);
 }
 
 TEST(HandRankTests, hasStraightFlushTest) {
@@ -238,28 +325,24 @@ TEST(HandRankTests, hasStraightFlushTest) {
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 406);
 }
 
-TEST(HandRankTests, hasStraightFlushTest2) {
-    vector<Card*> testCards;
-    Card* card = new Card(1, "Hearts", "One of Hearts", "♥");
-    Card* card6 = new Card(8, "Hearts", "Eight of Hearts", "♥");
-    Card* card1 = new Card(2, "Spades", "Two of Spades", "♠");
-    Card* card2 = new Card(3, "Spades", "Three of Spades", "♠");
-    Card* card3 = new Card(4, "Spades", "Four of Spades", "♠");
-    Card* card4 = new Card(5, "Spades", "Five of Spades", "♠");
-    Card* card5 = new Card(6, "Spades", "Six of Spades", "♠");
-    testCards.push_back(card);
-    testCards.push_back(card6);
-    testCards.push_back(card1);
-    testCards.push_back(card2);
-    testCards.push_back(card3);
-    testCards.push_back(card4);
-    testCards.push_back(card5);
+TEST(HandRankTests, hasStraightFlushTest2) { // Testing with real functions instead of stubhand
+    Hand* testHand = new Hand();
+    Card* card1 = new Card(5, "Diamonds", "Five of Diamonds", "♦");
+    Card* card2 = new Card(6, "Diamonds", "Six of Diamonds", "♦");
+    Card* card3 = new Card(7, "Diamonds", "Seven of Diamonds", "♦");
+    Card* card4 = new Card(8, "Diamonds", "Eight of Diamonds", "♦");
+    Card* card5 = new Card(2, "Spades", "Two of Spades", "♠");
+    Card* card6 = new Card(9, "Diamonds", "Nine of Diamonds", "♦");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
 
-    StubHand* testHand = new StubHand(testCards);
-    HandRank* handRanker = new HandRank();
+    vector<Card*> communityCards = {card3,card4,card5,card6};
     
-    ASSERT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 406);
+    testHand->calculateStrength(communityCards);
+    
+    ASSERT_EQ(testHand->getStrength(), 409);
 }
+
 
 TEST(HandRankTests, hasRoyalFlushTest) {
     vector<Card*> testCards;
@@ -284,27 +367,24 @@ TEST(HandRankTests, hasRoyalFlushTest) {
     EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 464);
 }
 
-TEST(HandRankTests, hasRoyalFlushTest2) {
-    vector<Card*> testCards;
-    Card* card1 = new Card(2, "Spades", "Two of Spades", "♠");
-    Card* card2 = new Card(13, "Diamonds", "King of Diamonds", "♦");
-    Card* card3 = new Card(10, "Diamonds", "Ten of Diamonds", "♦");
-    Card* card4 = new Card(11, "Diamonds", "Jack of Diamonds", "♦");
-    Card* card5 = new Card(12, "Diamonds", "Queen of Diamonds", "♦");
-    Card* card6 = new Card(3, "Diamonds", "Three of Diamonds", "♦");
-    Card* card7 = new Card(14, "Diamonds", "Ace of Diamonds", "♦");
-    testCards.push_back(card1);
-    testCards.push_back(card2);
-    testCards.push_back(card3);
-    testCards.push_back(card4);
-    testCards.push_back(card5);
-    testCards.push_back(card6);
-    testCards.push_back(card7);
 
-    StubHand* testHand = new StubHand(testCards);
-    HandRank* handRanker = new HandRank();
+TEST(HandRankTests, hasRoyalFlushTest2) { // Testing with real functions instead of stubhand
+    Hand* testHand = new Hand();
+    Card* card1 = new Card(9, "Spades", "Nine of Spades", "♠");
+    Card* card2 = new Card(10, "Diamonds", "Ten of Diamonds", "♦");
+    Card* card3 = new Card(11, "Diamonds", "Jack of Diamonds", "♦");
+    Card* card4 = new Card(12, "Diamonds", "Queen of Diamonds", "♦");
+    Card* card5 = new Card(2, "Spades", "Two of Spades", "♠");
+    Card* card6 = new Card(13, "Diamonds", "King of Diamonds", "♦");
+    Card* card7 = new Card(14, "Diamonds", "Ace of Diamonds", "♦");
+    testHand->addCard(card1);
+    testHand->addCard(card2);
+
+    vector<Card*> communityCards = {card3,card4,card5,card6,card7};
     
-    EXPECT_EQ(handRanker->getFinalRank(testHand->getCurrentHand()), 464);
+    testHand->calculateStrength(communityCards);
+    
+    ASSERT_EQ(testHand->getStrength(), 464);
 }
 
 // Pot Test Suite
