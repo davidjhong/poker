494b15a091769bfe6efc38a14a8355fc50086686
Chloe Tang <ctang085@xe-01.cs.ucr.edu>
2023-12-03 17:50:32 -0800

    editing test cases

--- tests/test.cpp
+++ tests/test.cpp
@@ -499,22 +499,6 @@ TEST(DisplayTest, displayGameStatusTest)
     "4. fold\n");
 }
 
-TEST(displayTest, displayMenuTest)
-{
-  ostringstream out; 
-  Display display; 
-  display.displayMenu(out);
-  EXPECT_EQ(out.str(),
-    "-------------- START MENU -----------------\n" 
-    "1) Start game\n"
-    "2) Settings\n" 
-    "3) Rules\n" 
-    "4) Card rankings\n" 
-    "5) Card combinations\n"
-    "q) Quit\n"
-    "Enter an option\n"
-    "-------------------------------------------\n");
-}
 
 TEST(displayTest, displayBetweenTurnsTest)
 {
@@ -529,30 +513,6 @@ TEST(displayTest, displayBetweenTurnsTest)
 }
 
 
-TEST(displayTest, displayRulesTest)
-{
-  ostringstream out;
-  Display displayru;
-  displayru.displayRules(out);
-  EXPECT_EQ(out.str(),
-    "\nOverview: \n" 
-    "Each player will be dealt two hole cards, followed by five community cards which will be dealt face up in intervals. \n"
-    "*Note that hole cards are cards that are kept face down throughtout the entire game, and can only be seen by the player that holds them. \n"
-    "The objective of the game is to make the best five-card poker hand using any combination of the player's hole cards and community cards\n"
-    "\nTexas Hold' em - in depth: \n"
-    "At the start of the round, before any cards are dealt, players will place their initial bets. The player to the dealer's left will be \nthe small blind, then the player to their left will be the big blind, which is double the value of the small blind. "
-    "2 hole cards will be dealt to each player. \nThe starting player may now chooute to fold, call or raise. \n"
-    "   fold: discard hand and put no more chips in the pot\n"
-    "   call: add the call amount to the pot\n"
-    "   raise: increase the call amount for the current round\n"
-    "Following this player's first move, each player will take their turn to call, raise or fold until every player has gone. After every player \nhas folded, called or raised, the dealer will deal the first 3 community cards face up. The players may now use these three cards to decide upon their next move. \n"
-    "Each player will again chooute to fold, call or raise. Then, the dealer will show the fourth community card, and each player will again chooute \nto fold, call or raise."
-    "Finally, the dealer will reveal the last community card. This fourth round of betting will continue until all players have folded, called or raised.\n"
-    "At this point, all remaining players will show their best hand from their two hole cards and 5 community cards. The player with the highest \nranked combination wins the pot.\n"
-    "If players tie, the highest hole card that isn't a part of their best hand is used to decide the winner.\n"
-    "q) back to menu \n");
-
-}
 
 // COMBO NAME TEST
 
